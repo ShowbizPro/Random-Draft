ebd78b1e3b178dba91893f290c672485cd16196f
ShowbizPro <robertsalex01@gmail.com>
2025-09-02 12:26:45 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const GITHUB_USERNAME = 'ShowbizPro';
+const REPO_NAME = 'Random-Draft';
+const FILE_PATH = 'result.json';
+const BRANCH = 'main';
+
+// If you're going to allow updates from frontend (not recommended), add a token here
+const GITHUB_TOKEN = ''; // Optional - better to update via GitHub Actions/server
